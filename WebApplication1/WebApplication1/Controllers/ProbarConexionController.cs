12c32d2457a8abb64d59b42a3f02dc3ff4fd82de
marioaje <marioaje@gmail.com>
2024-10-12 09:46:38 -0600

    Update ProbarConexionController.cs

--- WebApplication1/WebApplication1/Controllers/ProbarConexionController.cs
+++ WebApplication1/WebApplication1/Controllers/ProbarConexionController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
 
 namespace WebApplication1.Controllers
 {
@@ -8,6 +9,47 @@ namespace WebApplication1.Controllers
     [Route("api/[controller]")]
     public class ProbarConexionController : ControllerBase
     {
+        //Dos maneras de probar la conexion??
+        //Una es una consulta a Base de datos una tabla en especifica.
+        //Y la otra que es un test de conexion
+
+        //Declaracion de configuracion
+        private readonly IConfiguration _configuration;
+
+        //Inicializador con configuracion
+        public ProbarConexionController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        //Metodo ProbarConexion
+        [HttpGet]
+        public IActionResult ProbarConexion()
+        {
+            string connectionString = _configuration.GetConnectionString("Banco");
+
+            try
+            {
+                using ( var connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    return Ok("Conexion establecida");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            
+        }
+
+    //      "ConnectionString": {
+    //"ConnectionStrings": {
+    //  "Banco": "Server=saz-dev-mdb01;Port=3306;User=AKS1;Password=aKs1!ssvmnDEV;Database=AKS1;"
+    //}
+
+
+
        // private readonly BancoDbContext _context;
 
         //---select la tabla
